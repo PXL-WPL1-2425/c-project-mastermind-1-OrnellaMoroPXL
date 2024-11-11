562cafb437462c46b699024c3c3e0ef555240e25
OrnellaMoroPXL <12002643@student.pxl.be>
2024-11-11 12:48:43 +0100

    MasterMind-02
    add random generated solution to title

--- Mastermind/Mastermind/MainWindow.xaml.cs
+++ Mastermind/Mastermind/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,11 +24,72 @@ namespace Mastermind
         public MainWindow()
         {
             InitializeComponent();
+            GeneratedSolution();
         }
 
+        
+
+        private void GeneratedSolution()
+        {
+            Random randomColorGenerator = new Random();
+            string solution = "";
+
+            for (int i = 0; i < 4; i++)
+            {
+                int randomColorIndex = randomColorGenerator.Next(6);
+
+                switch (randomColorIndex)
+                {
+                    case 0:
+                        solution += "Red";
+                    break;
+
+                    case 1:
+                        solution += "Orange";
+                    break;
+
+                    case 2:
+                        solution += "Yellow";
+                    break;
+
+                    case 3:
+                        solution += "Green";
+                    break;
+
+                    case 4:
+                        solution += "Blue";
+                    break;
+
+                    case 5:
+                        solution += "White";
+                    break;
+
+                }
+                if (i < 3)
+                {
+                    solution += ", ";
+                }
+                
+
+                
+
+            }
+
+            this.Title = solution;
+
+
+
+        }
         private void ValidateAnswers_Click(object sender, RoutedEventArgs e)
         {
 
         }
+
+
+
+
+
+
+
     }
 }
